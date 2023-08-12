8dab8de8c4928b066eb1052a21a256782f219736
vijay-kumar-yadav <vky405021@gmail.com>
2023-08-12 08:20:41 +0530

    google analytics 4 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react';
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga4';
 
 const TRACKING_ID = "UA-204451421-1";
 ReactGA.initialize(TRACKING_ID);
 
 function App() {
   useEffect(() => {
+    console.log(window.pathname)
     ReactGA.pageview(window.location.pathname + window.location.search);
   }, []);
   return (
