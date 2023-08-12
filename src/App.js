5f5d5ec874ea8109f5ff9999343c5dbd2de76526
vijay-kumar-yadav <vky405021@gmail.com>
2023-08-12 08:10:30 +0530

    added analytics

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import ReactGA from 'react-ga';
+
+const TRACKING_ID = "UA-204451421-1";
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
   return (
-    <div>App</div>
+    <div>Testing...</div>
   )
 }
 
