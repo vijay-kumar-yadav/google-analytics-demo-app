3da90a234a504753952591ba270260568b2bcb1f
vijay-kumar-yadav <vky405021@gmail.com>
2023-08-12 08:42:11 +0530

    qr code added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, { useEffect } from 'react';
 import ReactGA from 'react-ga4';
+import qrCode from "./images/demo-app.png";
 
 const TRACKING_ID = "UA-204451421-1";
 ReactGA.initialize(TRACKING_ID);
 
 function App() {
   useEffect(() => {
-    console.log(window.pathname)
-    ReactGA.pageview(window.location.pathname + window.location.search);
+    ReactGA.send({
+      hitType: "pageview",
+      page: "/test-custom-path",
+      title: 'demo page',
+    });
   }, []);
   return (
-    <div>Testing...</div>
+    <>
+      <div>Demo App G analtics</div>
+      <img src={qrCode} alt="qr-code" width="200px" height="200px"/>
+    </>
   )
 }
 
